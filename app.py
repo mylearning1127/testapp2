5fe1527609c20b56a2099cff4da026f358267b01
mylearning1127 <110364459+mylearning1127@users.noreply.github.com>
2026-06-22 07:36:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello Python app containerization demo!!!!</h1>"
+    return "<h1>Hello Python app containerization demo version 2!!!!</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
